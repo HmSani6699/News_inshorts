921356467dc76707eb3960ad10323388b2d4b6a6
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-05-15 21:35:33 +0600

    added app and play story

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Navber from "../../../Navber/Navber";
+import appStore from "../../../assets/images/appstore.png";
+import playStore from "../../../assets/images/playstore.png";
 
 const Home = () => {
   return (
     <div>
       <Navber />
       {/* Home content */}
-      <div className="pt-[80px]">
-        <h2>Home content</h2>
+      <div className="pt-[140px]">
+        <div className="bg-[#f44336] text-white h-[70px] flex items-center justify-between px-[40px] max-w-[1200px] mx-auto">
+          <p className="">
+            For the best experience use{" "}
+            <span className="font-semibold">Inshorts</span> app on your
+            smartphone
+          </p>
+          <div className="flex gap-[20px]">
+            <img className="h-[45px]" src={appStore} alt="" />
+            <img className="h-[45px]" src={playStore} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
