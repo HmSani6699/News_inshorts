aa0ca11438f46153f71a7a960160a4b0cfe18fd6
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-05-31 01:25:08 +0600

    aded all text translate

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -4,8 +4,11 @@ import email from "../assets/images/email.svg";
 import facebook from "../assets/images/facebook.svg";
 import twitter from "../assets/images/twitter.svg";
 import likedin from "../assets/images/linkedin.svg";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="bg-[#323232] text-white py-[20px]  ">
       <div className="max-w-[890px] mx-auto h-[150px] flex flex-col lg:flex-row justify-center lg:justify-between items-center">
@@ -13,15 +16,20 @@ const Footer = () => {
           <div className=" border-r-[2px] text-right pr-[15px]">
             <img className="w-[170px]" src={footer_logo} alt="" />
             <p className="text-[16px] ">
-              Inshorts
-              <span className="text-[13px] text-[#808290]"> Pte. Ltd.</span>
+              {t("logo")}
+
+              <span className="text-[13px] text-[#808290]">
+                {t("footer.company")}
+              </span>
             </p>
-            <p className="text-[13px] text-[#808290]">© COPYRIGHT 2024 </p>
+            <p className="text-[13px] text-[#808290]">
+              {t("footer.copy_right")}
+            </p>
           </div>
           <div className="pl-[15px]">
-            <h2 className="text-[27px] font-bold">Contact Us</h2>
-            <p className="text-[13px]">Terms & conditions</p>
-            <p className="text-[13px]">Privacy Policies</p>
+            <h2 className="text-[27px] font-bold">{t("footer.contactUs")}</h2>
+            <p className="text-[13px]">{t("footer.terms")}</p>
+            <p className="text-[13px]">{t("footer.privacy")}</p>
           </div>
         </div>
         <div className="flex gap-[20px] mt-[20px] lg:mt-[0px]">
