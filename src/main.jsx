4df28a1de0b6c0245bb10894bc4de912bd888c3f
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-05-31 20:26:42 +0600

    added mew drag and light mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import "./index.css";
 import router from "./router/router.jsx";
 import { RouterProvider } from "react-router-dom";
 import "../src/it18n/it18n.js";
+import { ThemeProvider } from "./Theme/Theme.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <ThemeProvider>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </ThemeProvider>
 );
